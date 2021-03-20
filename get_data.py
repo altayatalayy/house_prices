c34e303f882702014ea60b3775a666d203b28eb7
Deniz Durdu <76404677+denizdurdu@users.noreply.github.com>
2021-03-20 18:57:21 +0300

    added files

---
+++ get_data.py
@@ -0,0 +1,7 @@
+
+import os
+
+os.system("kaggle competitions download -c house-prices-advanced-regression-techniques")
+os.system("unzip house-prices-advanced-regression-techniques.zip -d data")
+os.system("rm house-prices-advanced-regression-techniques.zip")
+
